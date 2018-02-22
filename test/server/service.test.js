3544b2131031dcd8d91057881f3174bc8fb24dc9
Matt Bontrager <matt.bontrager@gmail.com>
2018-02-22 13:45:46 -0800

    :white_check_mark: remove local test dependencies

--- test/server/service.test.js
+++ test/server/service.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const should = require('should');
+require('should');
+
 const request = require('supertest');
 const config = require('../../config');
 const service = require('../../server/service')(config);
@@ -12,37 +13,5 @@ describe('The Express service', () => {
 				.put('/foo')
 				.expect(404, done);
 		});
-	});
-});
-
-describe('PUT /service/:intent/:port', () => {
-	it('should return HTTP 200 with valid result', (done) => {
-		request(service)
-			.put('/service/test/9999')
-			.set('X-EMCEE-API-TOKEN', config.emceeApiToken)
-			.set('X-EMCEE-SERVICE-TOKEN', 'something')
-			.expect(200)
-			.end((err, res) => {
-				if (err) {
-					return done(err);
-				}
-				res.body.result.should.startWith('test at');
-				return done();
-			});
-	});
-
-	it('should return HTTP 403 with no API token provided', (done) => {
-		request(service)
-			.put('/service/test/9999')
-			.expect(403)
-			.end(done);
-	});
-
-	it('should return HTTP 400 with no service token provided', (done) => {
-		request(service)
-			.put('/service/test/9999')
-			.set('X-EMCEE-API-TOKEN', config.emceeApiToken)
-			.expect(400)
-			.end(done);
 	});
 });
